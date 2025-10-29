f032457c391e55840d235e9c9b2ba617b10628c2
Andy <4ndygeiss@gmail.com>
2025-10-29 12:42:47 +0100

    fix: change w,h

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -10,7 +10,7 @@ func main() {
 	engine.LoadImages("wisp-engine.png")
 	engine.AddEntity(engine.StateEntityAlive|engine.StateEntityVisible,
 		0, 0, 0,
-		128, 128,
+		320, 320,
 		engine.CanvasWidth/2, engine.CanvasHeight/2, 1,
 	)
 	engine.Run(func(dt float64) {
