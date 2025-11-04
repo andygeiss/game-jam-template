1d4757184915312f214a7c92b1f88517d54120d5
Andy <4ndygeiss@gmail.com>
2025-11-04 12:50:09 +0100

    refactor: rename spritesheet

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 const (
-	cols        = 30
-	rows        = 30
+	cols        = 22
+	rows        = 12
 	worldWidth  = float64(cols) * 32
 	worldHeight = float64(rows) * 32
 )
@@ -22,41 +22,28 @@ const (
 	indexImageTileset
 )
 
-// addPlayer adds the player entity to the game world.
-func addPlayer() {
-	engine.AddEntity(engine.StateEntityVisible,
-		indexImagePlayer, 0, 0,
-		32, 32,
-		worldWidth/2, worldHeight/2,
-		1,
-		1,
-	)
-}
-
-// addTiles adds the tiles to the game world.
-func addTiles() {
-	for i := 0; i < cols; i++ {
-		for j := 0; j < rows; j++ {
-			col := i % 2
-			row := j % 2
-			engine.AddEntity(engine.StateEntityVisible,
-				indexImageTileset, row, col,
-				32, 32,
-				float64(i)*32, float64(j)*32,
-				1,
-				0,
-			)
-		}
-	}
-}
-
 func main() {
 	// Load the assets.
-	engine.LoadImages("/assets/player.png", "/assets/tileset.png")
+	engine.LoadImages("/assets/spritesheet.png", "/assets/tileset.png")
 	engine.LoadSounds("/assets/title.ogg")
+	// Set the tilemap.
+	tilemap := []int{
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	}
 	// Add the entities.
 	addPlayer()
-	addTiles()
+	addTiles(tilemap)
 	// Start the game loop.
 	engine.Run(func(dt float64) {
 		// Apply camera shake when key 1 is pressed.
@@ -71,6 +58,9 @@ func main() {
 		if engine.Key3 {
 			engine.HitStopRemaining = 100
 		}
+		if engine.Key4 {
+
+		}
 	})
 	// Make an animation by using 8 frames with a duration of 100 ms each frame.
 	// This is handled by the engine under the hood.
@@ -82,4 +72,34 @@ func main() {
 	engine.SetWorldSize(worldWidth, worldHeight)
 	// Prevent the Go runtime from exiting.
 	select {}
+}
+
+// addPlayer adds the player entity to the game world.
+func addPlayer() {
+	engine.AddEntity(engine.StateEntityVisible,
+		indexImagePlayer, 0, 0,
+		32, 32,
+		worldWidth/2, worldHeight/2,
+		1,
+		1,
+	)
+}
+
+// addTiles adds the tiles to the game world.
+func addTiles(tiles []int) {
+	for i := 0; i < cols; i++ {
+		for j := 0; j < rows; j++ {
+			// Calculate the image column and row based on the tile index.
+			imgCol := tiles[i*rows+j] / cols
+			imgRow := tiles[i*rows+j] % cols
+			// Add the tile entity to the game world.
+			engine.AddEntity(engine.StateEntityVisible,
+				indexImageTileset, imgCol, imgRow,
+				32, 32,
+				float64(i)*32, float64(j)*32,
+				1,
+				0,
+			)
+		}
+	}
 }
